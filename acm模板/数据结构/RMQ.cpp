d860e451b3cb3686442082eb33c82b6f4913d216
ANiuZY <82268870+ANiuZY@users.noreply.github.com>
2021-04-10 20:31:33 +0800

    Create RMQ.cpp

---
+++ acm模板/数据结构/RMQ.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define M 10000
+using namespace std;
+int d[M][10]
+
+void RMQ_init(const vector<int>& A){
+    int n=A.size();
+    for(int i=0;i<n;++i)d[i][0]=A[i];
+    for(int j=1;(1<<j)<=n;++j)
+        for(int i=0;i+(1<<j)<=n;++i)
+            d[i][j]=min(d[i][j-1],d[i+(1<<(j-1))][j-1]);
+}
+int RMQ(int L,int R){
+    int k=0;
+    while((1<<(k+1))<=R-L+1)++k;
+    return min(d[L][k],d[R-(1<<k)+1][k]);
+}
